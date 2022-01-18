6e4a39414997fa441b894770a80352b37825865e
Oghenemaro Okolosio <Oghenemaro.Okolosio@stanbicibtc.com>
2022-01-18 13:51:20 +0100

    initiali project set up

---
+++ src/components/BackBtn.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { goBack } from '../utilis/utilis';
+</script>
+
+<p class="horizontal-center">
+  <!-- svelte-ignore a11y-missing-attribute -->
+  <strong><a role="button" class="link-2" on:click={goBack}>Back</a></strong>
+</p>
---
+++ src/components/Loadbtn.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let loadText;
+  export let loading;
+  export let loadFunc;
+</script>
+
+<div
+  class="button button-loadmore style2 js-bt-loading js-bt-loadmore"
+  on:click={loadFunc}>
+  {#if !loading}
+    <span class="txt-default">{loadText ? loadText : 'Load More'}</span>
+  {:else}
+    <span class="txt-default">Loading...</span>
+  {/if}
+</div>
